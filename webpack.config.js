5ada686990d404d79feef9b589bf46fe61396b30
Matthew Rothenberg <mrothenberg@gmail.com>
2016-09-02 17:45:17 -0400

    move config into file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,40 @@
 var path = require('path');
 var webpack = require('webpack');
 
-const isProduction = (process.env.NODE_ENV === 'production');
+const isProd = (process.env.NODE_ENV === 'production');
+
+function getPlugins() {
+    // using some tips from here: http://jonnyreeves.co.uk/2016/simple-webpack-prod-and-dev-config/
+    // but modified... (e.g. wtf why not just use EnvironmentPlugin?)
+    var plugins = [];
+
+    // Always expose NODE_ENV to webpack, you can now use `process.env.NODE_ENV`
+    // inside your code for any environment checks; UglifyJS will automatically
+    // drop any unreachable code.
+    plugins.push(
+        new webpack.EnvironmentPlugin(["NODE_ENV"])
+    );
+
+    
+    // Conditionally add plugins for Production builds.
+    if (isProd) {
+        plugins.push(
+            new webpack.NoErrorsPlugin(),
+            new webpack.optimize.UglifyJsPlugin({ 
+                compress: { warnings: false }
+            }),
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new webpack.optimize.DedupePlugin()
+        );
+    }
+
+    // Conditionally add plugins for Development
+    else {
+        // ...
+    }
+
+    return plugins;
+}
 
 module.exports = {
     entry: ['whatwg-fetch', './src/app.js'],
@@ -10,13 +43,16 @@ module.exports = {
         filename: 'bundle.js',
         publicPath: '/'
     },
+    
+    plugins: getPlugins(),
+    
     module: {
         loaders: [{
             test: /.jsx?$/,
             exclude: /node_modules/,
             loader: 'babel-loader',
             query: {
-                presets: isProduction ? ['es2015', 'react'] : ['es2015', 'react', 'react-hmre']
+                presets: isProd ? ['es2015', 'react'] : ['es2015', 'react', 'react-hmre']
             }
         }]
     }
