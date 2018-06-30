47da9e5489c11434137c9057effb4fde6287aa70
Matthew Rothenberg <mrothenberg@gmail.com>
2018-06-30 16:03:03 -0400

    disable serviceWorker caching until closer to launch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+
+/* serviceWorker is disabled to avoid caching in production while we're still
+   working on the initial version...
+   TODO: re-enable serviceWorker when/if closer to launch. */
+// import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
