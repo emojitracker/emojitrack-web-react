ace0e08de1fad32a365aeab0d29a7826a323a9a3
Matthew Rothenberg <mrothenberg@gmail.com>
2018-06-30 19:33:35 -0400

    store env variable the create_react_app way
    apparently it’s opinionated about this…

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import EmojiMatrix from "./components/Matrix";
 // import logo from './logo.svg';
 import "./App.css";
 
-/* TODO: there is probably a better way to import these now these days? */
-const REST_API = process.env.REST_API || "https://api.emojitracker.com/v1";
-const STREAM_API = process.env.STREAM_API || "http://stream.emojitracker.com";
-
 class App extends Component {
   render() {
-    return <EmojiMatrix source={REST_API + "/rankings"} />;
+    return (
+      <EmojiMatrix source={process.env.REACT_APP_REST_API + "/rankings"} />
+    );
   }
 }
 
