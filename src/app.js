fa09649c8458198a3100b4786fa71e95895ca696
Matthew Rothenberg <mrothenberg@gmail.com>
2018-06-30 14:50:56 -0400

    use new production REST api endpoint

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import EmojiMatrix from "./components/Matrix"
 import './App.css';
 
 /* TODO: there is probably a better way to import these now these days? */
-const REST_API   = process.env.REST_API   || "http://emojitracker.com/api"; // TODO: migrate to prod v1
+const REST_API   = process.env.REST_API   || "https://api.emojitracker.com/v1";
 const STREAM_API = process.env.STREAM_API || "http://stream.emojitracker.com";
 
 class App extends Component {
