6c60e009098063fd94d65d1204a51f95fe104f2d
Matthew Rothenberg <mrothenberg@gmail.com>
2018-06-30 15:48:00 -0400

    add and apply prettier to standardize formatting

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import EmojiMatrix from "./components/Matrix"
+import React, { Component } from "react";
+import EmojiMatrix from "./components/Matrix";
 
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 /* TODO: there is probably a better way to import these now these days? */
-const REST_API   = process.env.REST_API   || "https://api.emojitracker.com/v1";
+const REST_API = process.env.REST_API || "https://api.emojitracker.com/v1";
 const STREAM_API = process.env.STREAM_API || "http://stream.emojitracker.com";
 
 class App extends Component {
   render() {
-    return (
-      <EmojiMatrix source={REST_API + "/rankings"} />
-    );
+    return <EmojiMatrix source={REST_API + "/rankings"} />;
   }
 }
 
--- src/components/Matrix/index.js
+++ src/components/Matrix/index.js
@@ -1,46 +1,44 @@
-import React from 'react';
+import React from "react";
 import "./styles.css";
 
 class EmojiMatrix extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { data: [] };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
 
-    componentDidMount() {
-        var req = new Request(this.props.source);
-        fetch(req).then( (response) => {
-            response.json().then( (data) => {
-                this.setState({ data: data });
-            });
-        });
-    }
+  componentDidMount() {
+    var req = new Request(this.props.source);
+    fetch(req).then(response => {
+      response.json().then(data => {
+        this.setState({ data: data });
+      });
+    });
+  }
 
-    render() {
-        var matrixEntries = this.state.data.map( entry => {
-            const { id, score, char, name } = entry;
-            return <MatrixEntry key={id} score={score} char={char} name={name} />
-        });
+  render() {
+    var matrixEntries = this.state.data.map(entry => {
+      const { id, score, char, name } = entry;
+      return <MatrixEntry key={id} score={score} char={char} name={name} />;
+    });
 
-        return (
-            <div id="emojiMatrix">
-                <ul>
-                {matrixEntries}
-                </ul>
-            </div>
-        );
-    }
-};
+    return (
+      <div id="emojiMatrix">
+        <ul>{matrixEntries}</ul>
+      </div>
+    );
+  }
+}
 
 class MatrixEntry extends React.PureComponent {
-    render() {
-        return (
-            <li title={this.props.name}>
-                <span className="char">{this.props.char}</span>
-                <span className="score">{this.props.score}</span>
-            </li>
-        );
-    }
+  render() {
+    return (
+      <li title={this.props.name}>
+        <span className="char">{this.props.char}</span>
+        <span className="score">{this.props.score}</span>
+      </li>
+    );
+  }
 }
 
 export default EmojiMatrix;
