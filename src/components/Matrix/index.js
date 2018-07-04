83c1ff9bf164915b56fdb5c41586cb497f9404d9
Matthew Rothenberg <mrothenberg@gmail.com>
2018-07-03 22:57:55 -0400

    initial streaming test (#4)
    * WIP initial attempt at streaming
    * use async setState
    * even less comparison for should update.. i think
    * preserve matrix item ordering based on initial insert
    
    semi cribbing from the data structure in this post, which is pretty
    good for other stuff too:
    https://tech.smartling.com/react-redux-performance-and-the-benchmarks-to
    -prove-it-79b0bc9f25a4
    
    * try to fix issue with mutation
    * placeholder for animation
    * replicate some old terrible css
    * contribution guide placeholder
    * make stream src a prop of matrix llkats style
    * break streamEvent parsing into own method
    * switch streaming API to use SSL

--- src/components/Matrix/index.js
+++ src/components/Matrix/index.js
@@ -4,22 +4,74 @@ import "./styles.css";
 class EmojiMatrix extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+    this.state = { matrixItemIds: [], matrixItems: {} };
   }
 
   componentDidMount() {
     var req = new Request(this.props.source);
     fetch(req).then(response => {
-      response.json().then(data => {
-        this.setState({ data: data });
-      });
+      response
+        .json()
+        .then(data => {
+          let initialMatrixItemIds = [];
+          let initialMatrixItems = {};
+          for (const record of data) {
+            initialMatrixItemIds.push(record.id);
+            initialMatrixItems[record.id] = {
+              id: record.id,
+              name: record.name,
+              char: record.char,
+              score: record.score
+            };
+          }
+          this.setState({
+            matrixItemIds: initialMatrixItemIds,
+            matrixItems: initialMatrixItems
+          });
+        })
+        .then(() => {
+          this.startStreaming();
+        });
     });
   }
 
+  componentWillUnmount() {
+    this.stopStreaming();
+  }
+
+  parseStreamUpdate(data) {
+    const update = JSON.parse(data);
+    this.setState((prevState, props) => {
+      /* TODO: need to profile, but I suspect this is where some of the inefficiency is,
+      since updating state without mutation involves a heck of a lot of object creation
+      and I'm guessing maybe heap allocation / gc pressure? */
+      let updatedItems = {};
+      for (const [k, v] of Object.entries(update)) {
+        updatedItems[k] = { ...prevState.matrixItems[k] };
+        updatedItems[k].score += v;
+      }
+      const mergedItems = { ...prevState.matrixItems, ...updatedItems };
+      return { matrixItems: mergedItems };
+    });
+  }
+
+  startStreaming() {
+    this.scoreUpdates = new EventSource(this.props.stream);
+    this.scoreUpdates.onmessage = event => {
+      this.parseStreamUpdate(event.data);
+    };
+  }
+
+  stopStreaming() {
+    this.scoreUpdates.close();
+  }
+
   render() {
-    var matrixEntries = this.state.data.map(entry => {
-      const { id, score, char, name } = entry;
-      return <MatrixEntry key={id} score={score} char={char} name={name} />;
+    const matrixEntries = this.state.matrixItemIds.map(id => {
+      const c = this.state.matrixItems[id];
+      return (
+        <MatrixEntry key={c.id} score={c.score} char={c.char} name={c.name} />
+      );
     });
 
     return (
@@ -30,14 +82,39 @@ class EmojiMatrix extends React.Component {
   }
 }
 
-class MatrixEntry extends React.PureComponent {
+class MatrixEntry extends React.Component {
+  /* commented out code in this component is working animation placeholder,
+  preserving for the future but keeping off for now so we can focus on raw
+  perf before adding that whole complexity in! */
+  // constructor(props) {
+  //   super(props);
+  //   this.entryListItem = React.createRef();
+  // }
+
+  shouldComponentUpdate(nextProps, _nextState) {
+    return this.props.score !== nextProps.score;
+  }
+
   render() {
     return (
+      // <li title={this.props.name} ref={this.entryListItem}>
       <li title={this.props.name}>
-        <span className="char">{this.props.char}</span>
+        <span className="char emojifont">{this.props.char}</span>
         <span className="score">{this.props.score}</span>
       </li>
     );
+  }
+
+  componentDidUpdate() {
+    /* old animatin method, via timeout (no longer works well in chrome) */
+    // this.entryListItem.current.classList.add("highlight-score-update");
+    // setTimeout(() =>
+    //   this.entryListItem.current.classList.remove("highlight-score-update")
+    // );
+    /* alternate animation method, manually triggering reflow */
+    // this.entryListItem.current.classList.add("highlight-score-update");
+    // void this.entryListItem.current.offsetWidth;
+    // this.entryListItem.current.classList.remove("highlight-score-update");
   }
 }
 
